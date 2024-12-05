c6f456c6aef89aec64d3a5e7f040aac9c25db69e
cbjoed <cbjoedstrup@gmail.com>
2024-12-05 10:29:39 +0100

    ny model

--- UserServiceAPI/Models/User.cs
+++ UserServiceAPI/Models/User.cs
@@ -9,11 +9,15 @@ public class User
     [BsonRepresentation(BsonType.String)] // Ensures the Guid is stored as a string in MongoDB
     public Guid Id { get; set; }
 
-    public string? Username { get; set; }
-    public string? Password { get; set; }
-    public string? Address { get; set; }
-    public string? City { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string First_Name { get; set; }
+    public string Last_Name { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
     public int PostalCode { get; set; }
-    public string? Role { get; set; }
-    public string? TLF { get; set; }
+    public string Country { get; set; }
+    public string Role { get; set; }
+    public string Phone { get; set; }
 }
